97378585180a60749c936a54919b7208d41509e0
Shashi Nagarajan Iyer <shashiniyer@gmail.com>
2020-09-08 15:32:33 +0200

    removed examples from dijkstra function documentation

--- R/dijkstras.R
+++ R/dijkstras.R
@@ -5,10 +5,7 @@
 #' @param graph A dataframe with three numeric columns viz. "v1", "v2", and "w", representing each node, its adjacent node, and the distance between them, respectively
 #' @param init_node The starting node
 #' @return Shortest distance from \strong{\code{init_node}} to all other nodes in the \strong{\code{graph}}.
-#' @examples
-#' wiki_graph <- data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
-#' dijkstra(wiki_graph, 1)
-#' dijkstra(wiki_graph, 3)
+
 #' @references \href{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}{Dijkstra's Algorithm}
 #' @references \href{https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)}{Wiki page on what a graph is and more}
 
