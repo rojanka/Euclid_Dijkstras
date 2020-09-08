c5b9275ed43595c09f4d59a9d8516c6a8bbc7610
Shashi Nagarajan Iyer <shashiniyer@gmail.com>
2020-09-08 14:43:36 +0200

    Added documentation

---
+++ R/euclid.djikstras.R
@@ -0,0 +1,14 @@
+#' euclid.dijkstras
+#' 
+#' A package for two functions: one to compute the greatest common divisor of two numbers using Euclid's algorithm, another to compute the shortest distance between vertices in a graph using Dijkstra's algorithm
+#'
+#' The two functions in the package are:
+#' \strong{euclidean} and \strong{dijkstra}
+#' 
+#' @section More details available here:
+#' \code{\link{euclidean}}
+#' \code{\link{dijkstra}}
+#'
+#' @docType package
+#' @name euclid.dijkstras
+NULL
--- R/euclidean.R
+++ R/euclidean.R
@@ -1,3 +1,15 @@
+#' Get GCD of two numbers
+#' 
+#' Implements Euclid's algorithm to compute the greatest common divisor of two numbers
+#' 
+#' @param x A number
+#' @param y A number
+#' @return Greatest common divisor of \strong{\code{x}} and \strong{\code{y}}.
+#' @examples
+#' euclidean(123612, 13892347912)
+#' euclidean(100, 1000)
+#' @references \href{https://en.wikipedia.org/wiki/Euclidean_algorithm}{Euclidean Algorithm}
+
 euclidean <- function(x, y) {
   while (y!= 0) {
     t <- y
